329093883599fe6a87600984ce1aca2bbfd76f49
SjoerdGn <38759766+SjoerdGn@users.noreply.github.com>
2020-12-27 18:47:24 +0100

    eindelijk het probleempje met datetime opgelost

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from knmy import knmy
 import pandas as pd
+import datetime
 
 station = 260
 
@@ -20,17 +21,45 @@ station = st.sidebar.selectbox("Station", possible_stations,
                                index = 12,
                                 format_func=int)  
 
+startdate = st.sidebar.date_input('Start date', datetime.date(2015,11,1))
+start = datetime.datetime.combine(startdate,datetime.datetime.min.time())
+
+enddate = st.sidebar.date_input('End date', datetime.datetime(2020,12,1,0,0))
+end = datetime.datetime.combine(enddate,datetime.datetime.max.time())
+
 def plot_t_and_rh(station):
     disclaimer, stations, variables, data = knmy.get_knmi_data('hourly',
                             stations=[station],
+                            start = start.strftime('%Y%m%d%H'),
+                            end = end.strftime('%Y%m%d%H'),
                             parse=True) 
     
     
     
     data = data.iloc[1:]
     
-    data['YYYYMMDD'] = pd.to_datetime(data['YYYYMMDD'], format = '%Y%m%d')
-    data.index = data['YYYYMMDD']+pd.to_timedelta(data['HH'].astype(int), unit='h')
+    data['YYYYMMDD'] = pd.to_datetime(data['YYYYMMDD'], format = '%Y%m%d', errors='ignore')
+    
+    data.index = data['YYYYMMDD']+pd.to_timedelta(data['HH'].astype(int), unit='h',
+                                                  errors='ignore')
+    #data.index = pd.to_datetime(data.index, errors='ignore')
+    #data.index = data.index.tz_localize('Europe/Amsterdam', ambiguous = 'NaT')
+    droprows = ['2016-03-27 02:00:00',
+                '2016-10-30 02:00:00',
+                '2017-03-26 02:00:00',
+                '2017-10-29 02:00:00',
+                '2018-03-25 02:00:00',
+                '2018-10-28 02:00:00',
+                '2019-03-31 02:00:00',
+                '2019-10-27 02:00:00',
+                '2020-03-29 02:00:00',
+                '2020-10-25 02:00:00']
+    for row in droprows:
+        try:
+            data = data.drop(datetime.datetime.strptime(row, '%Y-%m-%d %H:%M:%S'), axis=0)
+        except:
+            pass
+    
     
     data = data[[ 'DD', 'FH', 'FF', 'FX', 'T', 'T10N', 'TD',
            'SQ', 'Q', 'DR', 'RH', 'P', 'VV', 'N', 'U', 'WW', 'IX', 'M', 'R', 'S',
@@ -38,14 +67,18 @@ def plot_t_and_rh(station):
     
     # RH filter
     data['RH'][data['RH'] < 0 ] = 0
+    data['RH'] = data['RH']/10
+    data['T'] = data['T']/10
     
     st.write(f"""
              ## Temperature at station {station} ({stations.name.values[0]})
+             in degrees Celcius
              """)
              
     st.line_chart(data['T'])
     st.write(f"""
              ## Precipitation at station {station} ({stations.name.values[0]})
+             in mm
              """)
     st.line_chart(data['RH'])
 
